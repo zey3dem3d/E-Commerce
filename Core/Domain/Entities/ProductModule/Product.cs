91ceb28c64fa40b3904de3dd786c9827080dc117
zey3dem3d <zeyademad987@gmail.com>
2025-12-04 23:22:25 +0200

    Add project files.

---
+++ Core/Domain/Entities/ProductModule/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.ProductModule
+{
+    public class Product : BaseEntity<int>
+    {
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string PictureUrl { get; set; } = null!;
+        public decimal Price { get; set; }
+
+        // One to Many Relationship Between Product & ProductBrand
+        public ProductBrand ProductBrand { get; set; }
+        public int BrandId { get; set; } // FK For ProductBrand
+
+        // One to Many Relationship Between Product & ProductType
+        public ProductType ProductType { get; set; }
+        public int TypeId { get; set; } // FK For ProductType
+    }
+}
---
+++ E-Commerce.API/Program.cs
@@ -0,0 +1,37 @@
+
+namespace E_Commerce.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            #region Configure Services
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(); 
+            #endregion
+
+            var app = builder.Build();
+
+            #region Configure Kestrel MiddelWares
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            #endregion
+
+            app.Run();
+        }
+    }
+}
